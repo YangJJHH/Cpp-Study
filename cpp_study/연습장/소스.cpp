8b876065a3aa17c160bffb772b0d37a4a87e3fc9
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2023-01-13 16:40:14 +0900

    연습

---
+++ cpp_study/연습장/소스.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <cmath>
+int main()
+{
+	using namespace std;
+	int a;
+	cout << "Á¤¼ö ÀÔ·Â:";
+	cin >> a;
+	if (a % 2==0) 
+	{
+		cout << "Â¦¼ö" << endl;
+	}
+	else
+	{
+		cout << "È¦¼ö"<<endl;
+	}
+}
