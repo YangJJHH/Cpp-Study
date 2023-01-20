0be32128ded54c5e5e43754fbfb5bc18793a2a30
JaeHyeock Yang <73065398+YangJJHH@users.noreply.github.com>
2023-01-20 16:53:40 +0900

    string, wstring

---
+++ cpp_study/string과wstring/소스.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	// basic - string
+	string string;
+	wstring wstring; // unsigned short << wide-Charater, Unicode
+	return 0;
+}
