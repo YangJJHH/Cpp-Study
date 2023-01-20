c72163f07d1dbffce4c7ac842f6ad649e88d5b98
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2023-01-20 22:57:41 +0900

    소멸자

---
+++ cpp_study/소멸자/소스.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+class Simple
+{
+private:
+	int _id;
+public:
+	Simple(const int& id) : _id(id)
+	{
+		cout << "생성자 입니다. id: " << _id << endl;
+	}
+
+	~Simple() {
+		cout << "소멸자 입니다 id: ." <<_id<< endl;
+	}
+};
+class ArrayClass
+{
+private:
+	int _len;
+	int* arr;
+public:
+	ArrayClass(const int& len) : _len(len)
+	{
+		arr = new int[_len];
+	}
+	~ArrayClass() {
+		delete[] arr; //소멸자가 실행되면서 알아서 메모리 해제
+	}
+
+};
+int main()
+{
+	//Simple s1(1);
+	//Simple* s3 = new Simple(3);
+	//Simple s2(2);
+	//delete s3;
+
+	while (true)
+	{
+		ArrayClass arr(1000); // 소멸자 실행 -> delete 메모리 해제
+	}
+	return 0;
+}
