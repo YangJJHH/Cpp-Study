62c90b00af69587f641cbcc5264e88c28f05ac46
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2023-02-08 00:02:49 +0900

    풀이완료

--- 백준 문제풀이/수들의 합2(2003번)/소스.cpp
+++ 백준 문제풀이/수들의 합2(2003번)/소스.cpp
@@ -4,9 +4,41 @@
 #include <string>
 #include <algorithm>
 using namespace std;
+int N, M;
 int main()
 {
+	//투 포인터
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	freopen("input.txt", "r", stdin);
+	cin >> N >> M;
+	int* arr = new int[N];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+	int left =0; // 구간의 왼쪽 포인터
+	int right =0; // 먼저 나아갈 구간의 오른쪽 포인터
+	int sum = arr[0];
+	int cnt = 0;
+	while (right < N)
+	{
+		if (sum < M) //합이 작다면 오른쪽을 한칸 더 움직임
+		{
+			right++;
+			sum += arr[right];
+		}
+		else if(sum == M)  //합이 같다면 더 이상 왼쪽을 볼 필요가 없기 때문에 왼쪽 한칸 증가
+		{
+			cnt++;
+			sum -= arr[left];
+			left++;
+		}
+		else if(sum>M) //합이 크다면 왼쪽을 한칸 더 움직여 구간을 줄임
+		{
+			sum -= arr[left];
+			left++;
+		}
+	}
+	cout << cnt;
 }
