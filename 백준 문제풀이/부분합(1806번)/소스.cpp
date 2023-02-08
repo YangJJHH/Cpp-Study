70488e2f5d2905b4caadbff31202f0186458e667
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2023-02-08 14:21:09 +0900

    풀이완료
    투 포인터

--- 백준 문제풀이/부분합(1806번)/소스.cpp
+++ 백준 문제풀이/부분합(1806번)/소스.cpp
@@ -4,5 +4,41 @@
 #include <string>
 #include <algorithm>
 #define fastio cin.tie(NULL)->sync_with_stdio(false);
-
+// 투 포인터
 using namespace std;
+
+int N, S,LENGTH = 1e5;
+
+int main()
+{
+	fastio;
+	freopen("input.txt","r",stdin);
+	cin >> N >> S;
+	vector<int> arr(N);
+	for (auto& ele : arr)
+	{
+		cin >> ele;
+	}
+
+	int left = 0;
+	int right = 0;
+	int sum = arr[0];
+	int flag = 0;
+	while (right<N)
+	{
+		if (sum < S)
+		{
+			right++;
+			if (right >= N) break;
+			sum += arr[right];
+		}
+		else
+		{
+			flag = 1;
+			LENGTH = min((right-left +1),LENGTH);
+			sum -= arr[left];
+			left++;
+		}
+	}
+	cout << (flag ? LENGTH : 0);
+}
