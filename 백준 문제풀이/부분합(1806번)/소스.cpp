76113cad54703e6fbf677eff95e4f4cf7d39f419
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2023-02-08 13:47:37 +0900

    부분합 (1806번)

---
+++ 백준 문제풀이/부분합(1806번)/소스.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#define fastio cin.tie(NULL)->sync_with_stdio(false);
+
+using namespace std;
