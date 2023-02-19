ab556b3b10f03a6d9b88b712b908d1cbb414bc1e
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2023-02-20 01:39:13 +0900

    풀이완료

--- 백준 문제풀이/스타트와링크(14889번)/소스.cpp
+++ 백준 문제풀이/스타트와링크(14889번)/소스.cpp
@@ -2,4 +2,78 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <set>
 #define fastio cin.tie(NULL)->sync_with_stdio(false);
+using namespace std;
+int N;
+int team_size;
+int arr[20][20]{ 0 };
+int visit[20]{ 0 };
+vector<int> start;
+int rst = 40000;
+void DFS(int idx)
+{
+	if (start.size() == team_size)
+	{
+		int start_sum=0;
+		int link_sum = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if (visit[i])
+			{
+				for (int j = 0; j < N; j++)
+				{
+					if (i == j) continue;
+					if (visit[j])
+					{
+						start_sum += arr[i][j];
+					}
+				}
+			}
+			else
+			{
+				for (int j = 0; j < N; j++)
+				{
+					if (i == j) continue;
+					if (!visit[j])
+					{
+						link_sum += arr[i][j];
+					}
+				}
+			}
+			
+		}
+		rst = min(abs(start_sum - link_sum), rst);
+		return;
+	}
+	for (int i = idx; i < N; i++)
+	{
+		if (!visit[i])
+		{
+			visit[i] = 1;
+			start.push_back(i);
+			DFS(i+1);
+			visit[i] = 0;
+			start.pop_back();
+		}
+	}
+	
+}
+int main()
+{
+	fastio;
+	freopen("input.txt", "r", stdin);
+	cin >> N;
+	team_size = N/ 2;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	DFS(0);
+	cout << rst;
+	return 0;
+}
