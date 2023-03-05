c3f41c1705da86b026432a54bbdf70994af48ef2
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2023-03-06 01:01:01 +0900

    풀이완료(DP)

--- 백준 문제풀이/계단오르기(2579번)/소스.cpp
+++ 백준 문제풀이/계단오르기(2579번)/소스.cpp
@@ -7,20 +7,26 @@ using namespace std;
 
 int N;
 
+
 int main()
 {
 	fastio;
 	freopen("input.txt", "r", stdin);
 	cin >> N;
-	vector<int> step(N);
-	for (int i = N-1; i >= 0; i--)
+	int step[301];
+	int DP[301];
+
+	for (int i =1; i <= N; i++)
 	{
 		cin >> step[i];
 	}
-
-	for (int i=0; i<N; i++)
+	DP[1] = step[1];
+	DP[2] = step[1] + step[2];
+	DP[3] = max(step[1]+step[3],step[2] + step[3]);
+	for (int i=4; i<=N; i++)
 	{
-		
+		DP[i] = max(DP[i-2]+step[i], step[i] + step[i-1] + DP[i-3]);
 	}
+	cout << DP[N];
 	return 0;
 }
