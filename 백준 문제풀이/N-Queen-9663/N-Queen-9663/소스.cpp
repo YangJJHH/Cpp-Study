ee36d70b3ca27449a621c72a54aaad4187869081
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2025-03-17 22:52:40 +0900

    N-Queen-9663

---
+++ 백준 문제풀이/N-Queen-9663/N-Queen-9663/소스.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int N;
+int rst = 0;
+int board[15];
+int check(int idx)
+{
+	for (int i = 0; i < idx; i++)
+	{
+		if (board[idx] == board[i] || abs(idx - i) == abs(board[idx] - board[i]))
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+void DFS(int cnt)
+{
+	if (cnt == N)
+	{
+		rst++;
+		return;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		board[cnt] = i;
+		if (check(cnt))
+		{
+			DFS(cnt + 1);
+		}
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N;
+	DFS(0);
+	cout << rst;
+	return 0;
+}
