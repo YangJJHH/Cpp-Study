28d096b0501ecfb19af6e08c5bae457fb2ba45b0
YangJJHH <73065398+YangJJHH@users.noreply.github.com>
2025-03-17 23:03:42 +0900

    N과M(3)-15651

---
+++ 백준 문제풀이/N과M(3)-15651/N과M(3)-15651/소스.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#define _TEST
+#include <cstring>	// for memset
+#include <limits.h>	// for INT_MAX
+#include <iostream>
+#include <string>
+#include <vector>
+#define fastio cin.tie(NULL)->sync_with_stdio(false);
+#define FILL(arr,val) fill(begin(arr), end(arr), val);
+#define CHECK_CONDITION_CONTINUE(condition) if(condition) continue;
+#define CHECK_CONDITION_BREAK(condition) if(condition) break;
+#define CHECK_CONDITION_RETURN_VOID(condition) if(condition) return;
+using namespace std;
+
+const int MAX = 8;
+int arr[MAX];
+int N, M;
+
+void solve(int k)
+{
+	if (k == M)
+	{
+		for (int i = 0; i < M; ++i)
+		{
+			cout << arr[i] + 1<<' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		arr[k] = i;
+		solve(k + 1);
+	}
+}
+
+int main()
+{
+	fastio;
+#ifndef BOJ
+	freopen("input.txt", "r", stdin);
+#endif // _TEST
+	cin >> N >> M;
+	solve(0);
+
+	return 0;
+}
